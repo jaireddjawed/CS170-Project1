132c83771ee77e5b65b4669cb82f0223a4307bd4
Jaired Jawed <jairedjawed@icloud.com>
2022-04-28 12:00:01 -0700

    Can choose choice of algorithm

--- main.py
+++ main.py
@@ -27,5 +27,25 @@ class Startup(object):
       else:
         puzzleChoice = input('That choice was not valid. Please type "1" to use a default puzzle or "2" to enter your own puzzle: ')
 
+    algoChoice = input('Select your choice of algorithm by entering the number associated: ')
+
+    print('1) Uniform Cost Search')
+    print('2) A* with the Misplaced Tile Heuristic')
+    print('3) A* with the Eucledian Distance Heuristic')
+
+    while True:
+      if algoChoice == "1":
+        break
+      elif algoChoice == "2":
+        break
+      elif algoChoice == "3":
+        break
+      else:
+        algoChoice = input('Your input was not valid. Select your choice of algorithm by entering the number associated: ')
+
+        print('1) Uniform Cost Search')
+        print('2) A* with the Misplaced Tile Heuristic')
+        print('3) A* with the Eucledian Distance Heuristic')
+
 if __name__ == '__main__':
   Startup().main()
