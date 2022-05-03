f9090e473f91cb41e5e24326550e9c55180907ba
Jaired Jawed <jairedjawed@icloud.com>
2022-05-03 11:28:36 -0700

    Implemented misplaced and euclidean heuristic

--- Problem.py
+++ Problem.py
@@ -5,12 +5,31 @@ from Node import Node
 
 class Problem(object):
   def __init__(self, initialState):
+    # the initial puzzle that the user chooses
     self.initalState = initialState
+
+    # the puzzle we want to reach from the user's chosen puzzle
     self.goalState = [
       ['1', '2', '3'],
       ['4', '5', '6'],
       ['7', '8', '0']
     ]
+
+    '''
+    possible goal states for reference (depends on the test cases used)
+
+    [
+      ['1', '2', '3'],
+      ['4', '5', '6'],
+      ['7', '8', '0']
+    ]
+
+    [
+      ['0', '1', '2'],
+      ['3', '4', '5'],
+      ['6', '7', '8']
+    ]
+    '''
 
     # start time of the algorithm
     self.startTime = time()
@@ -21,6 +40,8 @@ class Problem(object):
     # will hold all of the nodes that are currently in the frontier already
     self.alreadyInFrontier = set()
 
+    # only one of these can be true at any given time
+    # determines what type of search we are using
     self.uniform = False
     self.misplaced = False
     self.euclidean = False
@@ -29,17 +50,20 @@ class Problem(object):
     self.goalState = goalState
 
   def uniformCostSearch(self):
+    # set the search algorithm to ucs and start the search algorithm
     self.uniform = True
     self.graphSearch()
 
   def misplacedSearch(self):
+    # set the search algorithm to A* with misplaced heuristic and start the search algorithm
     self.misplaced = True
     self.graphSearch()
 
   def euclideanSearch(self):
+    # set the search algorithm to euclidean and start the search algorithm
     self.euclidean = True
     self.graphSearch()
-    
+
   def graphSearch(self):
     # queue to visit the nodes in order
     frontier = PriorityQueue()
@@ -48,10 +72,12 @@ class Problem(object):
     firstNode = Node(self.initalState)
     frontier.put(firstNode)
 
+    # we already have one node in the frontier and we've already expanded one node
     maxFrontierSize = 1
     numNodesExpanded = 1
 
     while True:
+      # there's a failure if the frontier is ever empty
       if frontier.qsize() == 0:
         print('Failure!\n')
         break
@@ -62,13 +88,15 @@ class Problem(object):
       print('The best state to expand with a g(n) = {} and h(n) = {} is ... '.format(currentNode.getGn(), currentNode.getHn()))
       currentNode.print()
 
+      # when we reach a puzzle that matches the goal state, we terminate the loop and print out
+      # the statistics that were required to reach the goal state
       if currentNode.getGrid() == self.goalState:
         endTime = time()
         timeToRun = endTime - self.startTime
 
         print('Goal!')
         print('Number of nodes expanded: {}.'.format(numNodesExpanded))
-        print('The maximum number of nodes in the queue at any one time: ' + str(maxFrontierSize) + '.')
+        print('The maximum number of nodes in the queue at any one time: {}.'.format(maxFrontierSize))
         print('Depth of the goal node: {}.'.format(currentNode.getGn()))
         print('Time to finish: {} second(s).'.format(timeToRun))
         break
@@ -87,24 +115,65 @@ class Problem(object):
             frontier.put(neighbor)
             self.alreadyInFrontier.add(neighbor)
             numNodesExpanded += 1
-        
+
         if self.misplaced:
           if not neighbor in self.alreadyInFrontier:
             misplacedCount = 0
+
+            # count the number of nodes that are in the incorrect place
+            # we will use it to set the heuristic
 
             for i in range(len(neighbor.getGrid())):
               for j in range(len(neighbor.getGrid()[0])):
                 if neighbor.getGrid()[i][j] != self.goalState[i][j]:
                   misplacedCount += 1
 
-            neighbor.setFn(misplacedCount)
+            # h(n) is the number of misplaced nodes
+            neighbor.setHn(misplacedCount)
+            # f(n) = g(n) + h(n)
+            neighbor.setFn(currentNode.getGn() + currentNode.getHn())
+
             frontier.put(neighbor)
             self.alreadyInFrontier.add(neighbor)
             numNodesExpanded += 1
 
-          if self.euclidean:
-            pass
+        if self.euclidean:
+          if not neighbor in self.alreadyInFrontier:
+            euclideanDistance = 0
 
+            row, col = 0, 0
+            goalRow, goalCol = 0, 0
+
+            # total number of numbers within grid (used to make it easy to convert it to a puzzle of a different length)
+            totalNumsInGrid = 9
+
+            # we go through the numbers within the grid and find how far each number is from where it is supposed to be in the goal state
+            num = 0
+            while num < totalNumsInGrid:
+              for i in range(len(neighbor.getGrid())):
+                  for j in range(len(neighbor.getGrid()[i])):
+                      if int(neighbor.getGrid()[i][j]) == num:
+                        row = i
+                        col = j
+
+                      if int(self.goalState[i][j]) == num:
+                          goalRow = i
+                          goalCol = j
+
+              # add up how far the number is on the row and column lengths
+              euclideanDistance += abs(goalRow-row) + abs(goalCol-col)
+              num += 1
+
+            # h(n) is the euclidean distance
+            neighbor.setHn(euclideanDistance)
+            # f(n) = g(n) + h(n)
+            neighbor.setFn(currentNode.getGn() + currentNode.getHn())
+
+            frontier.put(neighbor)
+            self.alreadyInFrontier.add(neighbor)
+            numNodesExpanded += 1
+
+      # if the frontier is larger than the max size we've seen so far, we replace the old size
       if frontier.qsize() > maxFrontierSize:
           maxFrontierSize = frontier.qsize()
 
@@ -116,10 +185,14 @@ class Problem(object):
 
     # check if a neighbor is above the currentNode
     if (i-1) >= 0:
+      # we can't use a shallow copy because it will modify the original grid and ruin it for the other neighbors
       temp = deepcopy(grid)
+
+      # swap the * grid cell with the number that exists above it
       temp[i][j], temp[i-1][j] = temp[i-1][j], temp[i][j]
       tempNode = Node(temp)
 
+      # do not add the node to the neighbors list if we've already checked it
       if not tempNode in self.visited:
         self.visited.add(tempNode)
         tempNode.setGn(currentNode.getGn() + 1)
